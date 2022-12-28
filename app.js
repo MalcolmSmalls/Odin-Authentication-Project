63ef4fe42b3c2cb7bcb25cad705a9cd0a2a7030b
MalcolmSmalls <smallsbeats@gmail.com>
2022-12-28 14:08:22 -0800

    Can now log in and log out

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Stategy;
+const LocalStrategy = require("passport-local")
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+
+
 
 const mongoDb = "mongodb+srv://shoot4themoon:Kq4m2iCZDthkMzSZ@cluster0.hmredok.mongodb.net/?retryWrites=true&w=majority"
 
@@ -28,11 +31,42 @@ app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUnitialized: true}));
 
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        if (user.password !== password) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      });
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+	res.render("index", { user: req.user });
+});
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"))
 
@@ -48,5 +82,25 @@ app.post("/sign-up", (req, res, next) => {
         }
     })
 })
+
+app.post(
+	"/log-in",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/"
+	})
+
+);
+
+app.get("/log-out", (req, res, next) => {
+	req.logout(function (err) {
+		if(err){
+			return next(err);
+		}
+		res.redirect("/");
+	});
+
+});
+
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
